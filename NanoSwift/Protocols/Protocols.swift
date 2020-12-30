af12710ecc1eae6a72f5770a9eba405527aa68e7
JMateusSousa <mateus_sousa_peu@hotmail.com>
2020-12-30 19:26:31 -0300

    Adding delegate changes

--- NanoSwift/Protocols/Protocols.swift
+++ NanoSwift/Protocols/Protocols.swift
@@ -26,59 +26,74 @@ protocol Professor {
 }
 
 protocol CampusDelegate {
-    func campusRequestProf(campus: String, area: String)
+    func campusRequestProf(campus: String, area: String, professor: IFCEProfessor)
 }
 
-class Campus {
+class Campus: CampusDelegate {
+
     let campusName: String
-    var delegate: CampusDelegate?
-    
+    var campusCalls: Int = 0
+    var fifoProfessor: [IFCEProfessor] = []
+
     init(campus: String) {
         self.campusName = campus
     }
-    
+
+    func campusRequestProf(campus: String, area: String, professor: IFCEProfessor) {
+
+        if campusCalls > 0 {
+            if campus == professor.campusRelocation && area == professor.area {
+                professor.changeCampus(campus: campus)
+
+                //  para evitar que o professor seja remanejado para
+                //  o atual campus e área dele.
+                //  Caso o campus solicite outro professor da mesma área.
+                professor.campusRelocation = ""
+                campusCalls -= 1
+            }
+        } else {
+            fifoProfessor.append(professor)
+        }
+    }
+
     func requestProf(area: String) {
-        delegate?.campusRequestProf(campus: campusName, area: area)
         print("\(self.campusName) requer professor da área de \(area)")
+        if fifoProfessor.isEmpty {
+            campusCalls += 1
+        } else {
+            let professor = fifoProfessor.first
+            fifoProfessor.removeFirst()
+            professor?.changeCampus(campus: professor!.campusRelocation)
+        }
     }
 }
 
-
 /*  Na classe IFCEProfessor seguimos o padrão de qualquer professor
     da face da terra (protocolo Professor) */
-class IFCEProfessor: Professor, CampusDelegate {
+class IFCEProfessor: Professor {
     var name: String
     var ID: Int
     var campus: String
     var area: String
-    var campusRelocation: String?
-    
+    var campusRelocation: String = ""
+    var delegate: CampusDelegate?
+
     init(name: String, ID: Int, campus: String, area: String) {
         self.name = name
         self.ID = ID
         self.campus = campus
         self.area =  area
     }
-    
+
     func requestRelocation(campus: String) {
         self.campusRelocation = campus
         print("\(self.name) deseja ir para o campus \(campus)")
-    }
-    
-    func campusRequestProf(campus: String, area: String) {
-        if campus == self.campusRelocation && area == self.area {
-            self.changeCampus(campus: campus)
-
-            //  para evitar que o professor seja remanejado para
-            //  o atual campus e área dele.
-            //  Caso o campus solicite outro professor da mesma área.
-            self.campusRelocation = ""
-        }
+        delegate?.campusRequestProf(campus: campus, area: area, professor: self)
     }
 
     func changeCampus(campus: String) {
         self.campus = campus
         print("\(self.name) foi relocado para o campus \(campus)")
     }
-    
+
 }
